ff74cd885b53b6000624e36d8299e50ebef9c378
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-06-07 19:40:41 +0530

    album components created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import AddAlbum from './components/AddAlbum';
+import SearchAlbum from './components/SearchAlbum';
+import DeleteAlbum from './components/DeleteAlbum';
+import ViewAll from './components/ViewAll';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddAlbum/>
-      
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<AddAlbum/>}/>
+    <Route path='/search' element={<SearchAlbum/>}/>
+    <Route path='/delete' element={<DeleteAlbum/>}/>
+    <Route path='/viewall' element={<ViewAll/>}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
